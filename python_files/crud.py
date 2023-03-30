bfebd9f77b84f413974e0e614492b7ececea8598
Maxime ROWELL <maximer@MacBook-Air-de-Maxime.local>
2023-03-30 13:58:42 +0200

    minor update

--- python_files/crud.py
+++ python_files/crud.py
@@ -1,7 +1,9 @@
 import sqlite3
 import datetime
 
+#######################################################################################################
 #############################################   CREATE    #############################################
+#######################################################################################################
 
 def new_user(nom:str, email:str, mdp:str, jwt:str) -> int:
     connexion = sqlite3.connect("bdd.db")
@@ -28,17 +30,7 @@ def user_follows(following_user_id : int, followed_user_id : int):
     connexion.commit()
     connexion.close()
 
-def user_unfollows(unfollowing_user_id : int, unfollowed_user_id : int):
-    connexion = sqlite3.connect("bdd_trading.db")
-    cursor = connexion.cursor()
 
-    cursor.execute("""
-                    DELETE FROM user_follows_user
-                        WHERE following_user_id = ? AND followed_user_id = ?
-                    """, (unfollowing_user_id, unfollowed_user_id))
-    
-    connexion.commit()
-    connexion.close()
 
 def new_action(entreprise : str, prix : int):
     connexion = sqlite3.connect("bdd_trading.db")
@@ -67,6 +59,79 @@ def new_transaction(user_id: int, action_id: int,
     connexion.close()
 
 
+
+
+def creer_article(titre:str, contenu:str, auteur_id:int) -> None: ####### à fusionner avec new_action()
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    curseur.execute("""
+                    INSERT INTO article 
+                        VALUES (NULL, ?, ?, ?, ?)
+                    """, (titre, contenu, datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S"), auteur_id))
+    # En savoir plus sur les dates : http://www.python-simple.com/python-modules-autres/date-et-temps.php
+    connexion.commit()
+    connexion.close()
+
+#######################################################################################################
+#############################################    READ     #############################################
+#######################################################################################################
+
+def get_jwt_by_email_mdp(email:str, mdp:str):
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    curseur.execute("""
+                    SELECT jwt FROM utilisateur WHERE email=? AND mdp=?
+                    """, (email, mdp))
+    resultat = curseur.fetchone()
+    connexion.close()
+    return resultat
+    
+# def get_users_by_mail(mail:str):
+#     connexion = sqlite3.connect("bdd.db")
+#     curseur = connexion.cursor()
+#     curseur.execute("""
+#                     SELECT * FROM utilisateur WHERE email=?
+#                     """, (mail,))
+#     resultat = curseur.fetchall()
+#     connexion.close()
+#     return resultat
+
+def get_id_user_by_email(email:str):
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    curseur.execute("""
+                    SELECT id FROM utilisateur WHERE email=?
+                    """, (email,))
+    resultat = curseur.fetchone()
+    connexion.close()
+    return resultat
+
+    
+def get_users_actions(id_user:int) -> list:
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    curseur.execute("""
+                    SELECT * FROM article WHERE auteur_id = 1
+                    """, (id_user,))
+    resultat = curseur.fetchall()
+    connexion.close()
+    return resultat
+
+#######################################################################################################
+#############################################   UPDATE   ##############################################
+#######################################################################################################
+
+def update_token(id, token:str)->None:
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    curseur.execute("""
+                    UPDATE utilisateur
+                        SET jwt = ?
+                        WHERE id=?
+                    """,(token, id))
+    connexion.commit()
+    connexion.close()
+
 def update_transaction(prix_vente:float, action_id:int):
     connexion = sqlite3.connect("bdd_trading.db")
     curseur = connexion.cursor()
@@ -80,68 +145,18 @@ def update_transaction(prix_vente:float, action_id:int):
     connexion.commit()
     connexion.close()
 
+#######################################################################################################
+#############################################   DELETE   ##############################################
+#######################################################################################################
 
-def creer_article(titre:str, contenu:str, auteur_id:int) -> None: ####### à fusionner avec new_action()
-    connexion = sqlite3.connect("bdd.db")
-    curseur = connexion.cursor()
-    curseur.execute("""
-                    INSERT INTO article 
-                        VALUES (NULL, ?, ?, ?, ?)
-                    """, (titre, contenu, datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S"), auteur_id))
-    # En savoir plus sur les dates : http://www.python-simple.com/python-modules-autres/date-et-temps.php
+def user_unfollows(unfollowing_user_id : int, unfollowed_user_id : int):
+    connexion = sqlite3.connect("bdd_trading.db")
+    cursor = connexion.cursor()
+
+    cursor.execute("""
+                    DELETE FROM user_follows_user
+                        WHERE following_user_id = ? AND followed_user_id = ?
+                    """, (unfollowing_user_id, unfollowed_user_id))
+    
     connexion.commit()
     connexion.close()
-    
-#############################################    READ   #############################################
-
-def get_jwt_depuis_email_mdp(email:str, mdp:str):
-    connexion = sqlite3.connect("bdd.db")
-    curseur = connexion.cursor()
-    curseur.execute("""
-                    SELECT jwt FROM utilisateur WHERE email=? AND mdp=?
-                    """, (email, mdp))
-    resultat = curseur.fetchone()
-    connexion.close()
-    return resultat
-    
-def get_users_by_mail(mail:str):
-    connexion = sqlite3.connect("bdd.db")
-    curseur = connexion.cursor()
-    curseur.execute("""
-                    SELECT * FROM utilisateur WHERE email=?
-                    """, (mail,))
-    resultat = curseur.fetchall()
-    connexion.close()
-    return resultat
-
-def get_id_user_by_email(email:str):
-    connexion = sqlite3.connect("bdd.db")
-    curseur = connexion.cursor()
-    curseur.execute("""
-                    SELECT id FROM utilisateur WHERE email=?
-                    """, (email,))
-    resultat = curseur.fetchone()
-    connexion.close()
-    return resultat
-
-def update_token(id, token:str)->None:
-    connexion = sqlite3.connect("bdd.db")
-    curseur = connexion.cursor()
-    curseur.execute("""
-                    UPDATE utilisateur
-                        SET jwt = ?
-                        WHERE id=?
-                    """,(token, id))
-    connexion.commit()
-    connexion.close()
-    
-
-def obtenir_article_user(id_user:int) -> list:
-    connexion = sqlite3.connect("bdd.db")
-    curseur = connexion.cursor()
-    curseur.execute("""
-                    SELECT * FROM article WHERE auteur_id = 1
-                    """, (id_user,))
-    resultat = curseur.fetchall()
-    connexion.close()
-    return resultat
